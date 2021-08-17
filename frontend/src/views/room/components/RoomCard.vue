f4552229f86fc99764f075d8ec6c7d5c7a53d929
Minji Kim <mjkim103301@gmail.com>
2021-08-17 17:59:40 +0900

    fix - 대시보드에서 history모드일때 방 대기화면 안뜨게 수정
    move-on-hover 속성 제거

--- frontend/src/views/room/components/RoomCard.vue
+++ frontend/src/views/room/components/RoomCard.vue
@@ -5,32 +5,17 @@
       class="card card-background move-on-hover background-wrap"
       v-bind:style="backgroundImg"
       style="position: relative"
+      v-if="!isHistory"
     >
       <div
         class="card-body content text-white ps-0"
-        v-if="!isHistory"
         data-bs-toggle="modal"
         :data-bs-target="'#RoomReadyModal' + roomInfo.room_id"
         style="cursor: pointer"
       >
-        <!-- <span style="display: inline-block"> -->
         <h4 class="text-white" style="cursor: pointer; float: left">
           {{ roomInfo.name }}
         </h4>
-        <!-- <p
-            v-if="!isHistory"
-            class="mb-2 card-join"
-            data-bs-toggle="modal"
-            :data-bs-target="'#RoomReadyModal' + roomInfo.room_id"
-          >
-            <i
-              class="fas fa-sign-in-alt"
-              style="color: #ffffff; font-size: 2rem"
-            ></i>
-          </p> -->
-        <!-- </span>
-        <span v-if="!isHistory"><br /></span> -->
-        <!-- <p class="mb-2 text-sm">Room Id</p> -->
         <p class="mb-2 text-lg" :class="{ cardPt5: isNow }">
           {{
             roomInfo.description.length > 30
@@ -51,6 +36,51 @@
 
         <!-- 방 번호로 api요청, Room Info->아이콘으로 교체 -->
         <p class="mb-4 text-sm" v-if="isHistory">
+          종료 :
+          {{
+            roomInfo.endTime.slice(0, 16).replace(/\-/g, '.').replace('T', ' ')
+          }}
+        </p>
+      </div>
+      <span
+        class="card-info"
+        @click.prevent="setAndGoToRoomInfo"
+        type="button"
+        style="z-index: 5"
+      >
+        <i class="fas fa-info-circle" style="z-index: 5"></i>
+      </span>
+    </div>
+    <div
+      class="card card-background background-wrap"
+      v-bind:style="backgroundImg"
+      style="position: relative"
+      v-if="isHistory"
+    >
+      <div class="card-body content text-white ps-0" style="cursor: pointer">
+        <h4 class="text-white" style="cursor: pointer; float: left">
+          {{ roomInfo.name }}
+        </h4>
+        <p class="mb-2 text-lg" :class="{ cardPt5: isNow }">
+          {{
+            roomInfo.description.length > 30
+              ? roomInfo.description.slice(0, 29)
+              : roomInfo.description
+          }}
+        </p>
+
+        <p class="mb-2 text-lg">
+          시작 :
+          {{
+            roomInfo.startTime
+              .slice(0, 16)
+              .replace(/\-/g, '.')
+              .replace('T', ' ')
+          }}
+        </p>
+
+        <!-- 방 번호로 api요청, Room Info->아이콘으로 교체 -->
+        <p class="mb-4 text-sm">
           종료 :
           {{
             roomInfo.endTime.slice(0, 16).replace(/\-/g, '.').replace('T', ' ')
