2e60ae850683f8984eac126d47c23896d4902fd9
DONGWOO SOHN <samsohn0128@gmail.com>
2021-08-19 17:45:20 +0900

    feat - 애니메이션을 지정하기 전에는 애니메이션이 없음.

--- frontend/src/store/modules/meetingRoom.js
+++ frontend/src/store/modules/meetingRoom.js
@@ -120,8 +120,7 @@ export default {
       state.currentPage = 0;
       state.location = state.location === null ? 'right' : state.location;
       state.size = state.size === null ? '2' : state.size;
-      state.transition =
-        state.transition === null ? 'fadein' : state.transition;
+      state.transition = state.transition === null ? 'none' : state.transition;
     },
     SET_ONGOING_PRESENTATION(state, { message, imageSrcs }) {
       state.imageSrcs = imageSrcs;
